de90e15ca146afb4dd127529a5e87fdea459faa2
Mucahit Kacmaz <mucahit@telemetrytv.com>
2023-05-09 15:47:40 +0300

    edited hello message for testing webhook

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hello again from TelemetryTV!" />
+      <HelloWorld msg="Hello again from TelemetryTV! This will be updated via WebHook!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
